dcdf4a8bf6434525bf56425876c8fa1acece7e00
Harrison Baghurst <070806harrison@gmail.com>
2026-02-05 13:34:10 +0000

    updated hero with squiggles

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,6 +2,7 @@
 
 import Button from '@/components/Button';
 import gsap from 'gsap';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation'
 import { useEffect, useRef, useState } from 'react';
 import { v4 as uuidv4 } from 'uuid'
@@ -48,42 +49,62 @@ const page = () => {
     }, [wordIndex])
 
     return (
-        <div className='bg-background w-dvw h-dvh flex items-center justify-center'>
-            <div className=''>
-                <div className='flex justify-center'>
-                    <div className='flex flex-col items-center justify-center gap-12'>
-                        <h1 className='font-mont-bold text-4xl bg'>
-                            Your
-                            <span className='inline-flex overflow-hidden py-3  m-3 w-44 justify-center'>
-                                {currentWord.split('').map((char, i) => (
-                                    <span
-                                        key={`${char}-${i}`}
-                                        ref={(el) => {
-                                            if (el) lettersRef.current[i] = el;
-                                        }}
-                                        className='inline-block'
-                                    >
-                                        {char}
-                                    </span>
-                                ))}
-                            </span>
-                            tool
-                        </h1>
+        <div className='bgmin-h-dvh'>
+            <div className='bg-background w-dvw h-dvh flex items-center justify-center overflow-hidden'>
+                <div className='w-dvw h-dvh absolute top-0 left-0 dotted-paper z-0'>
+                    <div className='absolute left-[10%] bottom-[30%] w-100 h-20'>
+                        <Image
+                            src={'/icons/squiggle1.svg'}
+                            alt='squiggle'
+                            fill
+                            className='rotate-50'
+                        />
+                    </div>
+                    <div className='absolute right-[10%] top-[30%] w-100 h-20'>
+                        <Image
+                            src={'/icons/squiggle2.svg'}
+                            alt='squiggle'
+                            fill
+                            className='rotate-30'
+                        />
                     </div>
                 </div>
-                <div className='flex justify-center gap-8 my-[2dvh]'>
-                    <Button
-                        text='Create workspace'
-                        handleClick={createBoard}
-                        variant='primary'
-                        icon='/icons/plus.svg'
-                    />
-                    <Button
-                        text='Join workspace'
-                        handleClick={() => { }}
-                        variant='secondary'
-                        icon='/icons/search.svg'
-                    />
+                <div className='bg-background/50 shadow-[0_0_40px_rgba(9,9,6,0.5)] rounded-full p-20 z-100'>
+                    <div className='flex justify-center'>
+                        <div className='flex flex-col items-center justify-center gap-12'>
+                            <h1 className='font-mont-bold text-4xl'>
+                                Your
+                                <span className='inline-flex overflow-hidden py-3  m-3 w-44 justify-center'>
+                                    {currentWord.split('').map((char, i) => (
+                                        <span
+                                            key={`${char}-${i}`}
+                                            ref={(el) => {
+                                                if (el) lettersRef.current[i] = el;
+                                            }}
+                                            className='inline-block'
+                                        >
+                                            {char}
+                                        </span>
+                                    ))}
+                                </span>
+                                tool
+                            </h1>
+                        </div>
+                    </div>
+                    <div className='flex justify-center gap-8 my-[2dvh]'>
+                        <Button
+                            text='Create workspace'
+                            handleClick={createBoard}
+                            variant='primary'
+                            icon='/icons/plus.svg'
+                        />
+                        <Button
+                            text='Join workspace'
+                            handleClick={() => { }}
+                            variant='secondary'
+                            icon='/icons/search.svg'
+                        />
+                    </div>
                 </div>
             </div>
         </div>
