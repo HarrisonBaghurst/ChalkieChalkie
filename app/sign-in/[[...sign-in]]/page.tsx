0830f493d38fafcd715841512ad74e242dd1abf2
Harrison Baghurst <070806harrison@gmail.com>
2026-02-18 21:00:43 +0000

    updated clerk sign in to center of page

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,12 +1,12 @@
-import { SignIn } from '@clerk/nextjs'
-import React from 'react'
+import { SignIn } from "@clerk/nextjs";
+import React from "react";
 
 const page = () => {
     return (
-        <div className='w-full h-full flex items-center justify-center'>
+        <div className="w-full h-dvh flex items-center justify-center">
             <SignIn />
         </div>
-    )
-}
+    );
+};
 
-export default page
+export default page;
