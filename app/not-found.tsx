b2fe211bd77ee6ec0907230a0ea2398fca6b399f
Harrison Baghurst <070806harrison@gmail.com>
2026-06-15 16:21:23 +0100

    updated not-found to match restyle

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Button from "@/components/Button";
+import Button from "@/components/dashboard/Button";
 import { useRouter } from "next/navigation";
 
 const notFound = () => {
@@ -11,18 +11,16 @@ const notFound = () => {
     };
 
     return (
-        <div className="w-dvw h-dvh flex items-center justify-center overflow-hidden">
+        <div className="w-dvw h-dvh flex items-center justify-center overflow-hidden dotted-paper">
             <div className="items-center flex flex-col gap-4">
-                <p className="text-display text-foreground">
-                    Got lost?
-                </p>
+                <p className="text-display text-foreground">Got lost?</p>
                 <p className="pb-4">
                     404 - The page your looking for doesn't exist
                 </p>
                 <Button
-                    text="Return home"
-                    variant="primary"
-                    handleClick={returnHome}
+                    text={"Return Home"}
+                    onClick={returnHome}
+                    size="large"
                 />
             </div>
         </div>
